7eb5ebfd0c2ed73965c49c735542783c9aed6a24
reotsugua <renanaugusto.ras@gmail.com>
2023-04-17 18:14:42 -0300

    primeito commit

--- Aula01M01/src/aula01/Main.java
+++ Aula01M01/src/aula01/Main.java
@@ -1,13 +1,19 @@
 package aula01;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Integer n =1;
+        Integer n =3;
+
+        List<Integer> lista = List.of(0,1,2,3,4,5,6,7,8,9,10);
+
+        lista.forEach((i)->System.out.println(n+" x "+i+" = "+n*i));
+
         
-        for (int i = 0; i < 11; i++) {
+        /*for (int i = 0; i < 11; i++) {
             System.out.println(n+" x "+i+" = "+n*i);
-        }
+        }*/
     }
 }
