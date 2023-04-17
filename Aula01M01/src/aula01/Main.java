6343f8d4e46374c90c78f809968e175a7afd4c80
reotsugua <renanaugusto.ras@gmail.com>
2023-04-17 17:37:06 -0300

    primeito commit

---
+++ Aula01M01/src/aula01/Main.java
@@ -0,0 +1,13 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer n =1;
+        
+        for (int i = 0; i < 11; i++) {
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
