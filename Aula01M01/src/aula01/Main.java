02c41e457304faaacd3a6ea42f9fb6892d2c7014
reotsugua <renanaugusto.ras@gmail.com>
2023-04-18 12:40:20 -0300

    Big Interger

--- Aula01M01/src/aula01/Main.java
+++ Aula01M01/src/aula01/Main.java
@@ -1,15 +1,29 @@
 package aula01;
 
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Integer n =3;
+        
+        Long numero6 = 5L;
+        numero6 = numero6+6;
+
+        System.out.println("numero6 = " + numero6);
+
+        BigInteger numeroBig = new BigInteger("1000000");
+        //numeroBig = numeroBig.add(new BigInteger(numero6.toString()));
+        BigInteger numBig = new BigInteger("11");
+        numeroBig = numeroBig.add(numBig);
+
+        System.out.println("numeroBig = " + numeroBig);
+        
+        /*Integer n =3;
 
         List<Integer> lista = List.of(0,1,2,3,4,5,6,7,8,9,10);
 
-        lista.forEach((i)->System.out.println(n+" x "+i+" = "+n*i));
+        lista.forEach((i)->System.out.println(n+" x "+i+" = "+n*i));*/
 
         
         /*for (int i = 0; i < 11; i++) {
