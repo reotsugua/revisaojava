90120be21ebe7eac8b320f66536faec29bc4f59c
reotsugua <renanaugusto.ras@gmail.com>
2023-04-22 18:07:19 -0300

    formatando saida

--- Aula01M01/src/aula01/Ex01.java
+++ Aula01M01/src/aula01/Ex01.java
@@ -10,12 +10,15 @@ public class Ex01 {
 
         BigInteger total = new BigInteger("0");
         total = total.add(janeiro).add(fevereiro).add(marco);
+        //BigInteger totalTri = janeiro.add(fevereiro).add(marco);
 
         System.out.printf("Janeiro = R$ %d\n", janeiro);
         System.out.printf("Fevereiro = R$ %d\n",fevereiro);
         System.out.printf("Março = R$ %d\n",marco);
         System.out.println("---------------------");
-        System.out.printf("TOTAL = R$ %d",total);
+        System.out.printf("TOTAL = R$ %,d",total);
+        //System.out.printf("O gasto total do trimestre foi: R$%,d.%n", total); O especificador de formato %d é usado para representar números inteiros, e o modificador , é usado para incluir separadores de milhares. O modificador %n é usado para incluir uma nova linha após a impressão da mensagem.
+
     }
     
 
