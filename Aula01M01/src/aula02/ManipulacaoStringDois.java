f29b3cc07d0b8bf038c7945634312be1956d2ca3
reotsugua <renanaugusto.ras@gmail.com>
2023-04-24 18:03:15 -0300

    aula 02 - Primeira letra do nome Maiuscula

--- Aula01M01/src/aula02/ManipulacaoStringDois.java
+++ Aula01M01/src/aula02/ManipulacaoStringDois.java
@@ -7,17 +7,22 @@ public class ManipulacaoStringDois {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Insira seu nome: ");
-        String nome = input.nextLine();
+        String nome = input.nextLine().toLowerCase();
 
         System.out.println("nome = " + nome);
 
-        String primeiraLetra = nome.substring(0,1);
+        //String primeiraLetra = nome.substring(0,1);
+        Character primeiraLetra = nome.toUpperCase().charAt(0);
         System.out.println("primeiraLetra = "+primeiraLetra);
         
         String restoNome = nome.substring(1);
         System.out.println("restoNome = " + restoNome);
         
-        String nomeFomatado = primeiraLetra.toUpperCase()+restoNome;
+        //String nomeFomatado = primeiraLetra.toUpperCase()+restoNome;
+        String nomeFomatado = primeiraLetra+restoNome;
         System.out.println("nomeFormatado = " + nomeFomatado);
+
+        System.out.println(nome.isEmpty());//nulo
+        System.out.println(nome.isBlank());//vazio
     }
 }
