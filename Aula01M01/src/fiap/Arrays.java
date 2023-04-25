7e50728a22295e7e78fcdc2c94a39d990c9c0baa
reotsugua <renanaugusto.ras@gmail.com>
2023-04-25 13:29:14 -0300

    arrays

---
+++ Aula01M01/src/fiap/Arrays.java
@@ -0,0 +1,4 @@
+package fiap;
+
+public class Arrays {
+}
---
+++ Aula01M01/src/fiap/For.java
@@ -0,0 +1,20 @@
+package fiap;
+
+public class For {
+    public static void main(String[] args) {
+        /* FOR
+        * Estrutura de repetição controlada por uma váriavel de contador
+        * Sintaxe básica:
+        * for (<inicialização>;<condição lógica>;<incremento ou decremento>)
+        *       { <Instruções>
+        * }
+        * for (int i = 0; i < ; i++) {
+        *
+        *  }
+        */
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
