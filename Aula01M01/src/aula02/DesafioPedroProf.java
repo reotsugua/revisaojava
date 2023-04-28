7f489010b42716b7891b8047e0b68fdc9cdaf581
reotsugua <renanaugusto.ras@gmail.com>
2023-04-28 16:29:55 -0300

    Desafio Pedro resolvido

--- Aula01M01/src/aula02/DesafioPedroProf.java
+++ Aula01M01/src/aula02/DesafioPedroProf.java
@@ -2,6 +2,13 @@ package aula02;
 
 public class DesafioPedroProf {
     public static void main(String[] args) {
+        String nome = "Pedro Bomfim Costa";
 
+        String[] parteNome = nome.split(" ");
+        parteNome [1] += " da";
+
+        nome = String.join(" ", parteNome); // Junta o array e coloca o Delimitador entre os itens do Array
+
+        System.out.println("nome = " + nome);
     }
 }
