5a4e17f1f90d07d9a1fb87fa43c85556b48a17e2
reotsugua <renanaugusto.ras@gmail.com>
2023-05-02 22:44:18 -0300

    Faça um programa que pegue o nome completo que o usuário digitar e imprima o nome com cada parte separada por espaço, um símbolo de exclamação no início. Exemplo:
    Nome: João Mario da Silva
    
    Resultado: !João !Maria !da !Silva

--- Aula01M01/src/aula02/Ex02.java
+++ Aula01M01/src/aula02/Ex02.java
@@ -1,4 +1,19 @@
 package aula02;
 
+import java.util.Scanner;
+
 public class Ex02 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira seu nome completo: ");
+        String nome = sc.nextLine();
+
+        String[] nomeSeparado = nome.split(" ");
+        for (int i = 0; i < nomeSeparado.length; i++) {
+            System.out.print("!"+nomeSeparado[i]+" ");
+        }
+    }
+
+
+
 }
