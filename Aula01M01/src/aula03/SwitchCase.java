7bd020f390d02e16829fe1cd41ea8bd76146e1a0
reotsugua <renanaugusto.ras@gmail.com>
2023-05-03 21:25:01 -0300

    SwitchCase

--- Aula01M01/src/aula03/SwitchCase.java
+++ Aula01M01/src/aula03/SwitchCase.java
@@ -9,15 +9,37 @@ public class SwitchCase {
         System.out.println("Digite sua faixa de idade: ");
         String tipoIdade = sc.nextLine();
 
-        switch (tipoIdade){
+        /*switch (tipoIdade){
             case "criança":
                 System.out.println("Você é uma criança");
+                break;
             case "adolescente":
                 System.out.println("Você é uma adolescente");
+                break;
             case "adulto":
                 System.out.println("Você é uma adulto");
+                break;
             case "idoso":
                 System.out.println("Você é um idoso");
+                break;
+            default:
+                System.out.println("Você é um E.T.");
+
+        }*/
+        switch (tipoIdade){
+            case "criança" ->
+                System.out.println("Você é uma criança");
+            case "adolescente" ->
+                System.out.println("Você é uma adolescente");
+            case "adulto" ->
+                System.out.println("Você é uma adulto");
+            case "idoso" ->
+                System.out.println("Você é um idoso");
+            default -> {
+                System.out.println("Você é um E.T.");
+                System.out.println("Você errou");
+            }
+
 
         }
     }
