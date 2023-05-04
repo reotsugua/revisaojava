1986a515bb413ebc8dc33fe5bf86bd2a729221c0
reotsugua <renanaugusto.ras@gmail.com>
2023-05-04 12:54:23 -0300

    Funç~oes

--- Aula01M01/src/aula03/Funcoes.java
+++ Aula01M01/src/aula03/Funcoes.java
@@ -1,4 +1,18 @@
 package aula03;
 
 public class Funcoes {
+    public static void main (String[] args){
+        Double v1 = 10d;
+        Double v2 = 5.5;
+
+        System.out.println(somaValores(v1, v2));
+
+    }
+
+    public static Double somaValores(Double n1, Double n2){
+        return n1 + n2;
+
+    }
+
+
 }
