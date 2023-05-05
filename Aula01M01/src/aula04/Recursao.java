34280359064f55ee901091ed078a6565d7a26b07
reotsugua <renanaugusto.ras@gmail.com>
2023-05-05 12:43:01 -0300

    Recursão

--- Aula01M01/src/aula04/Recursao.java
+++ Aula01M01/src/aula04/Recursao.java
@@ -5,14 +5,14 @@ import fiap.Arrays;
 public class Recursao {
     public static void main (String[] args){
         Integer[] array = {5,2,6,4,7,8};
-        array[0] = 0;
-        repetir(array,10);
+        //array[0] = 0;
+        repetir(array,2);
     }
 
-    public static void repetir(Integer[] repetNum, int fimNum){
-        if (repetNum[0] <= fimNum) {
-            System.out.println(repetNum[0]);
-            repetir(repetNum, fimNum);
+    public static void repetir(Integer[] lista, int repetNum){
+        if (lista.length > repetNum) {
+            System.out.println(lista[repetNum]);
+            repetir(lista, ++repetNum);
         }
     }
 }
